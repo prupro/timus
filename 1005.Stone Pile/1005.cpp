f16755a869c9ad84701533ef0da1f6a2a29d8a5b
prupro <pruviola@gmail.com>
2016-02-29 16:00:27 +0530

    1005 added. not submitted yet

---
+++ 1005.Stone Pile/1005.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+bool maxLessThan( int n, int w[], int halfDiff, int *index){
+	int max = 0;
+	int ind = 0;
+	
+	bool found = 0;
+	for(int i=0;i<n;i++){
+		if(w[i] >max && w[i] <= halfDiff ) {found = 1; max = w[i]; ind = i;}	
+	}	
+
+	*index = ind;
+
+	return found;
+}
+
+int main(){
+	int n;
+	int sum1=0, sum2=0;
+	cin >> n;
+	int w[n];
+	for(int i=0;i<n;i++){
+		cin >> w[i];
+		sum1 += w[i];		
+	}
+
+	int index=0;
+
+	while(	maxLessThan(n, w, (sum1-sum2)/2, &index) ){
+		sum1 -= w[index];
+		sum2 += w[index];
+		w[index] = 0;	
+
+	}	
+
+	cout << sum1-sum2;
+	return 0;
+}
---
+++ 1005.Stone Pile/problemStatement.md
@@ -0,0 +1,3 @@
+You have a number of stones with known weights w1, …, wn. Write a program that will rearrange the stones into two piles such that weight difference between the piles is minimal.
+
+http://acm.timus.ru/problem.aspx?space=1&num=1005
